caf8c5f415dc58e7cecd8feab56a38020aee677c
BJ Clark <bjclark13@outlook.com>
2020-11-30 21:58:22 -0500

    Add starter

---
+++ test/translate.test.js
@@ -0,0 +1,16 @@
+describe('translate', function () {
+	// tests go in here
+    //     Multiple tests for words starting with different vowels.
+    it("word starting with a", function() {
+        expect(translate('apple')).toEqual('appleway')
+    });
+    
+	// a. These tests are correct and pass.
+	// 2. At least one test for words starting with one consonant.
+	// a. These tests are correct and pass.
+	// 3. At least one test for words starting with two consonants.
+	// a. These tests are correct and pass.
+	// 4. At least one test for words starting with three consonants.
+	// a. These tests are correct and pass.
+	// 5. At least one test checking for lower case conversion.
+});
